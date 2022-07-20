716a53939583941ac70b086d423fe6f64ddaa779
bengris32 <bengris32@protonmail.ch>
2022-07-20 23:37:58 +0530

    X6833B: Introduce Mediatek In-Call Service
    * My attempt at fixing in call gain control on MediaTek devices.
    
    * It attempts to replicate what the stock MediaTek framework does
      when controlling the volume of the earpiece speaker, since MTK's
      audio HAL is not able to set the gain of the speaker from the
      values sent by AOSP framework.
    
    [LinkBoi00] Rebrand to Mediatek In-Call Service
    
    Change-Id: I43f727f6fb73a0d38457a41a8361653d1d00e220
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,10 @@ PRODUCT_PACKAGES += \
     libpcap.vendor \
     libtextclassifier_hash.vendor
 
+# MTK In-call service
+PRODUCT_PACKAGES += \
+    MtkInCallService
+    
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
