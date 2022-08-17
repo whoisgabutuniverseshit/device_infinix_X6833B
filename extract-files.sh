bc37f5637ac1e1281dbe3d895874f506b7292a92
bengris32 <bengris32@protonmail.ch>
2022-08-17 18:53:04 +0100

    X6833B: Patchelf C2 service to link against libavservices_minijail
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4cddc98e91724c86e7761fee41d4f9f5554c7203
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,9 @@ function blob_fixup {
         vendor/bin/hw/mt6789/camerahalserver)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
+            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
+            ;;
         vendor/lib*/hw/mt6789/vendor.mediatek.hardware.pq@2.15-impl.so)
             "$PATCHELF" --replace-needed "libutils.so" "libutils-v32.so" "$2"
             ;;
