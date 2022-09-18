8efd0beab139c46ddaa1107a3c9d41491c5fa0d0
Albert I <kras@raphielgang.org>
2022-09-18 12:11:23 +0530

    X6833B: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I7f337643927fdafe25d6701d588e5c24df39fff1
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -179,6 +179,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-com.mediatek.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-com.mediatek.ims.xml
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Init files
 PRODUCT_PACKAGES += \
     fstab.mt6789 \
