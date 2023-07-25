402de0513034639cfe3e133e59a239b97199a13a
bengris32 <bengris32@protonmail.ch>
2023-07-25 09:35:55 +0100

    X6833B: Add shim for proprietary AudioTrack constructor
    Change-Id: Icc7a1f9c748578f5b58499e7a76213673c4bcf54
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,9 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
+        lib64/libsink.so)
+            "${PATCHELF}" --add-needed "libshim_sink.so" "$2"
+            ;;
         vendor/lib*/hw/audio.primary.mt6789.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v31.so" "${2}"
             ;;
