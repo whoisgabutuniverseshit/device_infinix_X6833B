0a1f6778076246e88c1729536279296935eea12f
fjrXTR <fajarslebew31@gmail.com>
2024-06-30 08:35:37 +0000

    X6833B: fix power stuff * switch to prebuilt power service
    Change-Id: I5bd24eacc067955adfe19bd5b79b5360c8e573b0
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -344,14 +344,9 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-mediatek \
-    android.hardware.power-V2-ndk_platform.vendor \
-    android.hardware.power@1.0.vendor \
-    android.hardware.power@1.1.vendor \
-    android.hardware.power@1.2.vendor \
-    libmtkperf_client_vendor \
-    vendor.mediatek.hardware.mtkpower@1.0.vendor \
-    vendor.mediatek.hardware.mtkpower@1.1.vendor \
+    android.hardware.power@1.3.vendor
+
+PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 PRODUCT_COPY_FILES += \
